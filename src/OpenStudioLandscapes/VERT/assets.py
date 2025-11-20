fe6c066ba48bf845e8e6400fbf0cdb2e600921de
Michael Mussato <michimussato@gmail.com>
2025-11-20 14:09:24 +0100

    44-update-documentation: nox

--- src/OpenStudioLandscapes/VERT/assets.py
+++ src/OpenStudioLandscapes/VERT/assets.py
@@ -93,9 +93,9 @@ def compose_networks(
     Output[dict[str, dict[str, dict[str, str]]]] | AssetMaterialization, None, None
 ]:
 
-    compose_network_mode = ComposeNetworkMode.DEFAULT
+    compose_network_mode = DockerComposePolicies.NETWORK_MODE.DEFAULT
 
-    if compose_network_mode == ComposeNetworkMode.DEFAULT:
+    if compose_network_mode is DockerComposePolicies.NETWORK_MODE.DEFAULT:
         docker_dict = {
             "networks": {
                 "vert": {
