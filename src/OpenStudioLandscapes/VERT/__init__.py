7c8fbcb9793b025fa89cd1d0a8646037bc8e8dc5
Michael Mussato <michimussato@gmail.com>
2025-12-11 12:11:09 +0100

    47-refactor-to-config-based-landscapes: wip

--- src/OpenStudioLandscapes/VERT/__init__.py
+++ src/OpenStudioLandscapes/VERT/__init__.py
@@ -1,16 +1,19 @@
 import sys
+from pathlib import Path
+from importlib import metadata
 
-if sys.version_info[:2] >= (3, 8):
+if sys.version_info[:2] >= (3, 9):
     # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
+    from importlib.metadata import PackageNotFoundError, version, Distribution  # pragma: no cover
 else:
-    from importlib_metadata import PackageNotFoundError, version  # pragma: no cover
+    raise RuntimeError("Python version >= 3.9 required.")
 
 try:
     # Change here if project is renamed and does not equal the package name
-    dist_name = "OpenStudioLandscapes-VERT"
-    __version__ = version(dist_name)
+    package: str = Path(__file__).parent.parent.parent.parent.name
+    dist: Distribution = metadata.distribution(package)
+    __version__: str = version(dist.name)
 except PackageNotFoundError:  # pragma: no cover
-    __version__ = "unknown"
+    __version__: str = "unknown"
 finally:
     del version, PackageNotFoundError
