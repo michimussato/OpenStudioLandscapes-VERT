17561a7e81b9cb5708c711c233861927cd93df4b
Michael Mussato <michimussato@gmail.com>
2025-12-18 11:05:48 +0100

    47-refactor-to-config-based-landscapes: OpenStudioLandscapes-Flamenco updated

--- src/OpenStudioLandscapes/VERT/config/models.py
+++ src/OpenStudioLandscapes/VERT/config/models.py
@@ -1,3 +1,4 @@
+import enum
 import pathlib
 
 from dagster import get_dagster_logger
@@ -15,6 +16,10 @@ from OpenStudioLandscapes.VERT import dist
 
 config_default = pathlib.Path(__file__).parent.joinpath("config_default.yml")
 CONFIG_STR = config_default.read_text()
+
+
+class Branches(enum.StrEnum):
+    main = "main"
 
 
 class Config(FeatureBaseModel):
@@ -42,8 +47,9 @@ class Config(FeatureBaseModel):
     repository_url: HttpUrl = Field(
         default="https://github.com/VERT-sh/VERT.git",
     )
-    repository_branch: str = Field(
-        default="main",
+    repository_branch: Branches = Field(
+        default=Branches.main,
+        examples=[i.name for i in Branches],
     )
     repository_subdir: str = Field(
         default="VERT",
