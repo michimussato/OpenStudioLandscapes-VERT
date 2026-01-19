8f4d2523b7447ce5e17299b427ee55efbfc0af6c
Michael Mussato <michimussato@gmail.com>
2026-01-19 15:30:10 +0100

    - wip compose scope cmd

--- src/OpenStudioLandscapes/VERT/assets.py
+++ src/OpenStudioLandscapes/VERT/assets.py
@@ -21,13 +21,14 @@ from docker_compose_graph.utils import *
 from docker_compose_graph.yaml_tags.overrides import *
 from git.exc import GitCommandError
 from OpenStudioLandscapes.engine.common_assets.compose import get_compose
-from OpenStudioLandscapes.engine.common_assets.compose_scope import (
-    get_compose_scope_group__cmd,
-)
+# from OpenStudioLandscapes.engine.common_assets.compose_scope import (
+#     get_compose_scope_group__cmd,
+# )
 from OpenStudioLandscapes.engine.common_assets.docker_compose_graph import (
     get_docker_compose_graph,
 )
 from OpenStudioLandscapes.engine.common_assets.feature import get_feature__CONFIG
+from OpenStudioLandscapes.engine.common_assets.cmd import get_feature__cmd
 from OpenStudioLandscapes.engine.common_assets.feature_out import get_feature_out_v2
 from OpenStudioLandscapes.engine.common_assets.group_in import (
     get_feature_in,
@@ -51,7 +52,7 @@ yaml.SafeDumper.add_multi_representer(
 )
 
 
-compose_scope_group__cmd: AssetsDefinition = get_compose_scope_group__cmd(
+cmd: AssetsDefinition = get_feature__cmd(
     ASSET_HEADER=ASSET_HEADER,
 )
 
