398619ad079c1d81d3cab81c3ea60b128e4db86b
Michael Mussato <michimussato@gmail.com>
2026-04-11 18:42:27 +0200

    nox

--- README.md
+++ README.md
@@ -462,4 +462,4 @@ To follow up on the previous LinkedIn publications, visit:
 
 ***
 
-Last changed: **2026-04-11 11:25:46 UTC**
+Last changed: **2026-04-11 12:36:09 UTC**
--- src/OpenStudioLandscapes/VERT/_definitions_with_upstream_specs.py
+++ src/OpenStudioLandscapes/VERT/_definitions_with_upstream_specs.py
@@ -1,8 +1,7 @@
 from dagster import Definitions
-
-from OpenStudioLandscapes.VERT.definitions import assets_base
 from OpenStudioLandscapes.engine.base.assets import group_out_base_spec
 
+from OpenStudioLandscapes.VERT.definitions import assets_base
 
 assets_external = []
 assets_external.append(group_out_base_spec)
