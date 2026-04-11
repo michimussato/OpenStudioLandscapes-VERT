0122b68d26b3d7ef24fe630d4ceeb32984073f3c
Michael Mussato <michimussato@gmail.com>
2026-04-11 13:30:24 +0200

    implements individual development/testing/debugging functionality

--- src/OpenStudioLandscapes/VERT/definitions.py
+++ src/OpenStudioLandscapes/VERT/definitions.py
@@ -4,16 +4,14 @@ from dagster import (
 )
 
 import OpenStudioLandscapes.VERT.assets
-from OpenStudioLandscapes.engine.features.upstream_asset_specs import assets_external
 
-assets = load_assets_from_modules(
+assets_base = load_assets_from_modules(
     modules=[OpenStudioLandscapes.VERT.assets],
 )
 
 
 defs = Definitions(
     assets=[
-        *assets,
-        *assets_external,
+        *assets_base,
     ],
 )
