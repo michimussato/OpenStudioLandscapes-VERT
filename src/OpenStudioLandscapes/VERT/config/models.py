7716da9e8286cedc6d64e8325416189dfed5ce93
Michael Mussato <michimussato@gmail.com>
2026-04-26 21:07:49 +0200

    remove compute_fields. serializing them causes problems and is also not needed.

--- src/OpenStudioLandscapes/VERT/config/models.py
+++ src/OpenStudioLandscapes/VERT/config/models.py
@@ -7,7 +7,7 @@ from pydantic import (
     Field,
     HttpUrl,
     PositiveInt,
-    computed_field,
+    # computed_field,
 )
 
 LOGGER = get_dagster_logger(__name__)
@@ -63,7 +63,7 @@ class Config(FeatureBaseModel):
     # )
 
     # EXPANDABLE PATHS
-    @computed_field
+    # @computed_field
     @property
     def docker_compose_override_expanded(self) -> pathlib.Path:
         LOGGER.debug(f"{self.env = }")
