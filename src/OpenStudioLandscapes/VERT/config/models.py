3918c068c72376c3a248c0e306bffc6c268e91e7
Michael Mussato <michimussato@gmail.com>
2026-04-30 22:37:02 +0200

    option to set logging level for custom loggers

--- src/OpenStudioLandscapes/VERT/config/models.py
+++ src/OpenStudioLandscapes/VERT/config/models.py
@@ -9,7 +9,10 @@ from pydantic import (
     PositiveInt,
 )
 
+from OpenStudioLandscapes.cli import LOGGING_LEVEL_DEFAULT
+
 LOGGER = get_dagster_logger(__name__)
+LOGGER.setLevel(LOGGING_LEVEL_DEFAULT)
 
 from OpenStudioLandscapes.engine.config.models import FeatureBaseModel
 
@@ -81,4 +84,10 @@ class Config(FeatureBaseModel):
         return ret
 
 
-CONFIG_STR = Config.get_docs()
+if __name__ == "__main__":
+    CONFIG_STR = Config.get_docs()
+else:
+    import yaml
+    CONFIG_STR = yaml.dump(
+        Config.model_json_schema(mode="serialization"),
+    )
